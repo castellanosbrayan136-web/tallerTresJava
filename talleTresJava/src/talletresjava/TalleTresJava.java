8404fdf2a53f143e21df681549c3aeceaf2b16dc
castellanosbrayan136-web <castellanosbrayan136@gmail.com>
2026-02-18 14:42:36 -0500

    .
    Removed placeholder text from the main method.

--- talleTresJava/src/talletresjava/TalleTresJava.java
+++ talleTresJava/src/talletresjava/TalleTresJava.java
@@ -16,7 +16,6 @@ public class TalleTresJava {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        jfkdslllllllllllllllllllllllllllllllllllllllll
     }
     
 }
